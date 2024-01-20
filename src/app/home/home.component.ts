9213ae8c4cb8c0ae60c05ac7bfdd49fd6365d508
Makoorr <azizbenrejeb@gmail.com>
2024-01-20 21:58:38 +0100

    Removed unecessary initialisations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthenticationService } from '../services/authentification.service';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +6,5 @@ import { AuthenticationService } from '../services/authentification.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  email: string;
-  password: string;
-
-  constructor(private authService: AuthenticationService) {
-    this.email = '';
-    this.password = '';
-  }
-
-  onSubmit() {
-    this.authService.login(this.email, this.password);
-  }
+  
 }
