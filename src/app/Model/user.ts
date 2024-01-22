7abbba3f39d6c4231803d4d5ce564a36ed9ded40
jawherlaben <jawher.labben1999@gmail.com>
2024-01-22 19:01:58 +0100

    Add User Model

---
+++ src/app/Model/user.ts
@@ -0,0 +1,16 @@
+export interface User {
+    _id?: string;
+    firstname: string;
+    lastname: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+    dateBirth?: string;
+    appliedJobs?: string[];
+    gender?: string;
+    address?: string;
+    profileImage?: string;
+    role?: string;
+    date_added?: Date;
+  }
+  
