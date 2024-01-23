fcf575ab79cc30966e6368a9486aeec88df003b4
jawherlaben <jawher.labben1999@gmail.com>
2024-01-23 14:42:49 +0100

    Create Menu Class

---
+++ src/app/core/constants/menu.ts
@@ -0,0 +1,50 @@
+import { MenuItem } from "src/app/Model/menu-model/menu-model";
+
+
+export class Menu {
+  public static pages: MenuItem[] = [
+    {
+      group: 'Base',
+      separator: false,
+      items: [
+        {
+          icon: 'assets/icons/heroicons/outline/chart-pie.svg',
+          label: 'Dashboard',
+          route: '/dashboard',
+          children: [
+            { label: 'Jobs', route: '/' },
+            { label: 'Companies', route: '/' },
+          ],
+        },
+        {
+          icon: 'assets/icons/heroicons/outline/lock-closed.svg',
+          label: 'Auth',
+          route: '/auth',
+          children: [
+            { label: 'Sign up', route: '/' },
+            { label: 'Sign in', route: '/' },
+            { label: 'Forgot Password', route: '/' },
+            { label: 'New Password', route: '/' },
+            { label: 'Two Steps', route: '/' },
+          ],
+        },
+      ],
+    },
+    {
+      group: 'Config',
+      separator: false,
+      items: [
+        {
+          icon: 'assets/icons/heroicons/outline/cog.svg',
+          label: 'Settings',
+          route: '/settings',
+        },
+        {
+          icon: 'assets/icons/heroicons/outline/bell.svg',
+          label: 'Notifications',
+          route: '/',
+        },
+      ],
+    },
+  ];
+}
