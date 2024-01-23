eef43dcbafde8185027ec940af4b2c5e4fee8fae
jawherlaben <jawher.labben1999@gmail.com>
2024-01-23 14:47:39 +0100

    Create User service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,49 +1,58 @@
 import { Injectable } from '@angular/core';
 import { User } from '../Model/user';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
-import * as jwt_decode from "jwt-decode";
+import * as jwt_decode from 'jwt-decode';
 import { environment } from 'src/environments/environment.prod';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private userUrl = environment.apiUrl;
+  private currentUserSubject: BehaviorSubject<User | undefined>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User | undefined>(undefined);
+  }
+
+  getCurrentUser(): Observable<User | undefined> {
+    return this.currentUserSubject.asObservable();
+  }
+
+  private setUser(user: User | undefined) {
+    this.currentUserSubject.next(user);
+  }
 
   getUserById(userId: string): Observable<User> {
     const url = `${this.userUrl}/user/findUserbyId/${userId}`;
     return this.http.get<User>(url);
   }
 
-
-
-  getUserFromToken(): Observable<User | undefined> {
+  getUserFromToken(): void {
     const storedToken = localStorage.getItem('currentUserToken');
-  
+
     if (storedToken) {
       try {
         const userId = GetUserIdFromToken(storedToken);
-        return this.getUserById(userId);
+        this.getUserById(userId).subscribe(
+          user => {
+            this.setUser(user);
+          },
+          error => {
+            console.error('Erreur lors de la récupération de l\'utilisateur : ', error);
+          }
+        );
       } catch (error) {
         console.error('Erreur lors du décodage du token : ', error);
       }
+    } else {
+      this.setUser(undefined);
     }
-  
-    return new Observable(observer => observer.next(undefined));
   }
-
 }
 
 function GetUserIdFromToken(token: string): string {
-
   const decodedToken: any = jwt_decode.jwtDecode(token);
   return decodedToken._id;
 }
-
-
-
