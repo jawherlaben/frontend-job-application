e193c5d8e134586fb2247a508f0e201b31ae28f6
jawherlaben <jawher.labben1999@gmail.com>
2024-01-23 19:58:20 +0100

    Display user information on the sidebar

--- src/app/sidebar/sidebar-menu/sidebar-menu.component.ts
+++ src/app/sidebar/sidebar-menu/sidebar-menu.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { MenuService } from 'src/app/services/menu.service';
 import { SubMenuItem } from 'src/app/Model/menu-model/menu-model';
+import { User } from 'src/app/Model/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
     selector: 'app-sidebar-menu',
@@ -11,11 +13,19 @@ import { SubMenuItem } from 'src/app/Model/menu-model/menu-model';
    
 })
 export class SidebarMenuComponent implements OnInit {
-  constructor(public menuService: MenuService) {}
+  public user: User | undefined;
+
+  
+  constructor(public menuService: MenuService,private userService: UserService) {}
 
   public toggleMenu(subMenu: SubMenuItem) {
     this.menuService.toggleMenu(subMenu);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userService.getUserFromToken();
+    this.userService.getCurrentUser().subscribe(user => {
+    this.user = user;
+    });
+  }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -4,6 +4,8 @@ import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
 import { NgClass, NgIf } from '@angular/common';
 import { ThemeService } from '../theme.service';
 import { MenuService } from '../services/menu.service';
+import { User } from '../Model/user';
+import { UserService } from '../services/user.service';
 
 @Component({
     selector: 'app-sidebar',
@@ -12,14 +14,21 @@ import { MenuService } from '../services/menu.service';
  
 })
 export class SidebarComponent implements OnInit {
+  public user: User | undefined;
 
-  constructor(public themeService: ThemeService, public menuService: MenuService) {}
+  constructor(public themeService: ThemeService, public menuService: MenuService, private userService: UserService) {}
 
-  ngOnInit(): void {}
-
+  ngOnInit(): void {
+    this.userService.getUserFromToken();
+    this.userService.getCurrentUser().subscribe(user => {
+      this.user = user;
+    });
+  }
   public toggleSidebar() {
     this.menuService.toggleSidebar();
   }
 
  
 }
+
+
