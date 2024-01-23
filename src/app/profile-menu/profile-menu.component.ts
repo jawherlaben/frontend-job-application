b3066c123a88a7c64336324653a001d628fa4c7a
jawherlaben <jawher.labben1999@gmail.com>
2024-01-23 21:43:17 +0100

    Enable the logout functionality in the profile Menu

--- src/app/profile-menu/profile-menu.component.ts
+++ src/app/profile-menu/profile-menu.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 //import { ClickOutsideDirective } from '../shared/click-outside-directive.directive'
 import { NgClass } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { User } from '../Model/user';
 import { UserService } from '../services/user.service';
+import { AuthenticationService } from '../services/authentification.service';
 
 @Component({
   selector: 'app-profile-menu',
@@ -14,10 +15,33 @@ export class ProfileMenuComponent implements OnInit {
   public isMenuOpen = false;
   @Input() public user: User | undefined;
 
+  isUserLoggedIn = false; 
+  isCompanyUser = false; 
+  showDropdown = false;
 
-  constructor(private userService: UserService) {}
+  constructor(private authService: AuthenticationService,private userService: UserService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.authService.isLoggedIn.subscribe(loggedIn => {
+      this.isUserLoggedIn = loggedIn;
+      if (this.isUserLoggedIn) {
+        this.authService.isCompanyUser.subscribe(isCompanyUser => {
+          this.isCompanyUser = isCompanyUser;
+        });
+
+      this.userService.getUserFromToken();
+      this.userService.getCurrentUser().subscribe(user => {
+      this.user = user;
+      });
+      }
+    });
+
+    this.authService.redirectToHome.subscribe(redirectToHome => {
+      if (redirectToHome) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
 
   public toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
@@ -25,5 +49,12 @@ export class ProfileMenuComponent implements OnInit {
     this.userService.getCurrentUser().subscribe(user => {
       this.user = user;
     });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isUserLoggedIn = false;
+    this.isCompanyUser = false;
+    this.showDropdown = false;
   }
 }
