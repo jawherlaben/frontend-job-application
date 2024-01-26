9bdc1f2952f515df3f3ac7f6d94a26d5d0f56c94
Makoorr <azizbenrejeb@gmail.com>
2024-01-26 22:32:24 +0100

    fixed routing error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,46 +12,34 @@ import { UserSettingsComponent } from './user-settings/user-settings.component';
 import { UserNotificationsComponent } from './user/simple-user/user-notifications/user-notifications.component';
 import { ProflixVerifiedSettingsComponent } from './user-settings/proflix-verified-settings/proflix-verified-settings.component';
 import { PaiementsSettingsComponent } from './user-settings/paiements-settings/paiements-settings.component';
-
 import { JobComponent } from './job/job.component';
-
 import { SimpleUserComponent } from './user/simple-user/simple-user.component';
 import { UserSettingsAuthComponent } from './user-settings/user-settings-auth/user-settings-auth.component';
 import { UserProfilComponent } from './profile/user-profil/user-profil.component';
 
-
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-
   { path: 'job', component: JobComponent },
-  { path: 'profile', component: ProfileComponent },
-
 
   { path:'user-component', redirectTo: '/user-component/user-dashboard', pathMatch: 'full'},
   { path: 'user-component', component: SimpleUserComponent, children: [
     { path: 'profile', component: UserProfilComponent },
     { path: 'user-dashboard', component: UserDashboardComponent },
 
-
     { path: 'contact', component: ContactComponent },
-
     { path: 'user-settings', component: UserSettingsComponent },
     { path: 'auth-settings', component: UserSettingsAuthComponent },
     { path: 'payments-settings', component: PaiementsSettingsComponent },
     { path: 'proflix-verified-settings', component: ProflixVerifiedSettingsComponent },
     { path: 'user-notifications', component: UserNotificationsComponent },
-
     { path: 'company/:id', component: UserNotificationsComponent },
-
   ]},
-
 
   { path: 'contact', component: ContactComponent },
   { path: 'authentification', component: AuthentificationComponent },
   { path: 'companies', component: CompaniesComponent },
   { path: 'company-dashboard', component: CompanyDashboardComponent },
-
 
   { path: 'forbidden', component: AccessForbiddenComponent },
   { path: '**', component: NonExistentURLsComponent },
